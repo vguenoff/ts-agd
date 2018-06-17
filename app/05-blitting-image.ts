c705a6fbfb762a8f94248066b01e62b3ec453f3f
Vassil Guenoff <vguenoff@gmail.com>
2018-06-17 22:03:05 +0300

    Using sprites

--- app/05-blitting-image.ts
+++ app/05-blitting-image.ts
@@ -7,6 +7,14 @@ export class BlittingImage {
         canvas.setAttribute('height', '256');
         canvas.style.border = '1px solid black';
 
+        const tileset = new Image();
+        tileset.src = '../assets/images/tileset.png';
+        tileset.addEventListener('load', () => {
+            ctx.drawImage(tileset, 192, 128, 64, 64, 96, 96, 64, 64);
+            // the source x and y position; the source height and width;
+            // the source x and y position; the source height and width
+        });
+
         document.body.appendChild(canvas);
     }
 }

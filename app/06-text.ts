d36af6aaf1f2d36fd160157fb8a347b367fd4873
Vassil Guenoff <vguenoff@gmail.com>
2018-06-21 08:12:14 +0300

    Canvas chapter done

--- app/06-text.ts
+++ app/06-text.ts
@@ -7,6 +7,21 @@ export class Text {
         canvas.setAttribute('height', '256');
         canvas.style.border = '1px solid black';
 
+        const content = 'Hello world!';
+
+        ctx.font = "24px 'Rockwell Extra Bold','Futura',sans-serif";
+        const textWidth = ctx.measureText(content).width;
+        const textHeight = ctx.measureText('M').width;
+
+        ctx.fillStyle = 'red';
+        ctx.textBaseline = 'middle';
+
+        ctx.fillText(
+            content,
+            canvas.width / 2 - textWidth / 2,
+            canvas.height / 2 - textHeight / 2,
+        );
+
         document.body.appendChild(canvas);
     }
 }
